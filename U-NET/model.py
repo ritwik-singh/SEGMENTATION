de520c78424fb87549c1b23d323d44a47dc8a203
ritwik-singh <31265500+ritwik-singh@users.noreply.github.com>
2019-07-25 00:16:01 +0530

    Update model.py

--- U-NET/model.py
+++ U-NET/model.py
@@ -1,1 +1,18 @@
+'''
+ * @author [Ritwik Singh]
+ * @create date 2019-07-25 00:14:15	
+ * @desc [description]
+
+'''
+from __future__ import print_function,division
+
+import os
+import numpy as np 
+
+import keras
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 
